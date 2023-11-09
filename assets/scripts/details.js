8f96cc2864b77c2cc859baf84e36086925c59ad6
joana <joanaalvesm@gmail.com>
2023-11-09 15:14:51 -0300

    Initialized details page.

---
+++ assets/scripts/details.js
@@ -0,0 +1,36 @@
+function updateProductDetails(product) {
+    if (product) {
+        document.getElementById('productName').textContent = product.title;
+        document.getElementById('productImage').src = product.image;
+        document.getElementById('productDescription').textContent = product.description;
+        document.getElementById('productCategory').textContent = product.category;
+        document.getElementById('productPrice').textContent = product.price;
+
+        updateAdditionalImages(product.images);
+    } else {
+        alert("Product not found!")
+    }
+}
+
+// existe ou não?
+function updateAdditionalImages(images) {
+    const imageList = document.getElementById('imageList');
+    imageList.innerHTML = '';
+
+    images.forEach((imageURL) => {
+        const imageCol = document.createElement('div');
+        imageCol.className = 'col-md-4';
+        const image = document.createElement('img');
+        image.src = imageURL;
+        image.className = 'w-100 object-fit-cover';
+        image.height = '300';
+        
+    });
+}
+
+async function fetchProductDetails(productId){
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    const product = data.find((product) => product.id === parseInt(productId));
+    return product;
+}
